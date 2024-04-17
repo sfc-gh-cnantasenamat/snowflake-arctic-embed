097a6579cc779e28d08047e1fb081af26356ff65
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2024-04-17 09:59:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,4 +59,4 @@ for query, query_scores in zip(queries, scores):
     
     st.write("Query:", query)
     for document, score in doc_score_pairs:
-        st.write(score.float(), document)
+        st.write(score.dtype, document)
